8d6939d54de3402df383cb0eefe3d89a6a9c3447
zeyadmvtr <126705564+zeyadmvtr@users.noreply.github.com>
2026-03-06 14:35:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+
+model = load_model("model.h5")
+
+@app.route("/")
+def home():
+    return "Electricity Anomaly Detection API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json["input"]
+    data = np.array(data).reshape(1, -1)
+
+    prediction = model.predict(data)
+    result = int(prediction[0] > 0.5)
+
+    return jsonify({"prediction": result})
+
+if __name__ == "__main__":
+    app.run()
